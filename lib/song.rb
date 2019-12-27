772ece7c3c412a269c8241c5e92e224c3743f649
Antony Sanders <antony.devon92@gmail.com>
2019-12-27 15:40:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,15 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+ def self.create
+   song = self.new
+    @@all << song
+    song
+  end
+ 
   def self.all
     @@all
   end
@@ -9,5 +17,60 @@ class Song
   def save
     self.class.all << self
   end
+
+
+
+ def self.new_by_name(name)
+  #instantiates a song with a name property 
+  song = self.new
+  song.name = name
+  song
+ end
+ 
+ 
+ 
+ def self.create_by_name(name)
+   #instantiates and saves a song with a name property
+   #binding.pry 
+   song = self.new
+   song.name = name
+   song
+   @@all << song
+   @@all
+ end 
+ 
+ 
+ 
+ def self.find_by_name(name)
+   #can find a song present in @@all by name
+   #returns falsey when a song name is not present in @@all
+   #binding.pry
+   @@all.detect do |song| 
+     if song.name == name
+      return song
+     else 
+       return nil 
+     end
+  end
+   
+ end 
+ 
+ 
+ def self.find_or_create_by_name
+   
+ end 
+ 
+ def self.alphabetical(name)
+   @@all.each do |song|
+     song.name.sort 
+   end
+ end 
+ 
+ 
+ def self.destroy_all
+   @@all.clear
+ end 
+ 
+
 
 end
