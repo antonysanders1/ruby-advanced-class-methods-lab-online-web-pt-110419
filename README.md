6b3cfff607f930d81c85e706d395465b30abc6bf
Antony Sanders <antony.devon92@gmail.com>
2019-12-27 18:04:57 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ruby Advanced Class Methods Lab
+ with # Ruby Advanced Class Methods Lab
 
 ## Learning Goals
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,15 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+ def self.create
+   song = self.new
+    @@all << song
+    song
+  end
+ 
   def self.all
     @@all
   end
@@ -9,5 +17,68 @@ class Song
   def save
     self.class.all << self
   end
+
+
+
+ def self.new_by_name(name)
+  #instantiates a song with a name property 
+  song = self.new
+  song.name = name
+  song
+ end
+ 
+ 
+ 
+ def self.create_by_name(name)
+   #instantiates and saves a song with a name property
+   song = self.create
+   song.name = name
+   song
+ end 
+ 
+ 
+ 
+ def self.find_by_name(name)
+   #can find a song present in @@all by name
+   #returns falsey when a song name is not present in @@all
+   #binding.pry
+   @@all.detect { |song| song.name == name}
+
+ end 
+ 
+ 
+ def self.find_or_create_by_name(name)
+   self.find_by_name(name) || self.create_by_name(name)
+ end 
+ 
+ def self.alphabetical
+   @@all.sort_by {|song| song.name }
+ end 
+ 
+ def self.new_from_filename(filename)
+   #binding.pry
+   array = filename.split(" - ")
+   array[1] = array[1].split(".")[0]
+   #binding.pry
+   song = self.new 
+   song.name = array[1]
+   song.artist_name = array[0]
+   song
+ end 
+ 
+def self.create_from_filename(filename)
+  array = filename.split(" - ")
+   array[1] = array[1].split(".")[0]
+   #binding.pry
+   song = self.create 
+   song.name = array[1]
+   song.artist_name = array[0]
+   song
+end 
+ 
+ def self.destroy_all
+   @@all.clear
+ end 
+
 
 end
